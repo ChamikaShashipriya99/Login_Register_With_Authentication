c881c834ee5bf5044a278e8571c5ef1d7b978238
USER™ <chamikashashipriya3@mail.com>
2025-03-16 12:30:09 +0530

    ssss

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,12 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("HEllo World")
+    res.send("Hello Chamika")
 })
 
 app.listen(3000, () => {
-	console.log("Server is running on port: ", PORT);
+	console.log("Server is running on port 3000");
 });
+
+//chcDmdUBLFb9u6On MongoDB Password
+// mongodb+srv://chamikashashipriya3:chcDmdUBLFb9u6On@cluster0.nonya.mongodb.net/
